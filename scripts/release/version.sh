77f6191c92d205525a0f8176a388625936f8a0ad
ernestognw <ernestognw@gmail.com>
2023-01-05 19:09:18 -0600

    Fix release-pr

--- scripts/release/version.sh
+++ scripts/release/version.sh
@@ -2,12 +2,13 @@
 
 set -o errexit
 
+CHANGESETS_STATUS_JSON="./status.json"
+npx changeset status --output="$CHANGESETS_STATUS_JSON"
 RELEASES=$(node --print --eval "require('$CHANGESETS_STATUS_JSON').releases")
-if [ $(node --print --eval "$RELEASES.length") != 1 ]; then
-  echo "::error file=$CHANGESETS_STATUS_JSON::The status doesn't contain only 1 release"
-  exit 1;
-fi;
+npx changeset status --output="$CHANGESETS_STATUS_JSON"
+if [ $(node --print --eval "$RELEASES.length") != 1 ]; then exit 1; fi;
 NEW_VERSION=$(node --print --eval "($RELEASES)[0].newVersion")
+rm $CHANGESETS_STATUS_JSON
 
 # Simulates the idea of updating the version comment in OZ contracts
-echo NEW_VERSION > .version
+echo $NEW_VERSION > .version
