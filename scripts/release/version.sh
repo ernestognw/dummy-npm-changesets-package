ce392f08bc96f0def8739a41816111b818c47087
ernestognw <ernestognw@gmail.com>
2023-01-05 20:01:17 -0600

    Again

--- scripts/release/version.sh
+++ scripts/release/version.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-set -euo pipefail -x
-
 # Simulates the idea of updating the version comment in OZ contracts
 echo $(node --print --eval "require('./package.json').version") > .version
