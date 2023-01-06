2f009823343943ccdf1b68d9d77a2c06f70b71d8
ernestognw <ernestognw@gmail.com>
2023-01-05 19:54:57 -0600

    Try again

--- scripts/release/version.sh
+++ scripts/release/version.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit
-
-CHANGESETS_STATUS_JSON="./status.json"
-npx changeset status --output="$CHANGESETS_STATUS_JSON"
-RELEASES=$(node --print --eval "require('$CHANGESETS_STATUS_JSON').releases")
-npx changeset status --output="$CHANGESETS_STATUS_JSON"
-if [ $(node --print --eval "$RELEASES.length") != 1 ]; then exit 1; fi;
-NEW_VERSION=$(node --print --eval "($RELEASES)[0].newVersion")
-rm $CHANGESETS_STATUS_JSON
+set -euo pipefail -x
 
 # Simulates the idea of updating the version comment in OZ contracts
-echo $NEW_VERSION > .version
+echo $(node --print --eval "require('./package.json').version") > .version
